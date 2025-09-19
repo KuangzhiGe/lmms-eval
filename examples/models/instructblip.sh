78a15e61a4c3064b2bb7242f1f37d51097057f9d
KuangzhiGe <2200013209@stu.pku.edu.cn>
2025-09-19 20:45:32 +0800

    Add PhysicalMind

--- examples/models/instructblip.sh
+++ examples/models/instructblip.sh
@@ -1,7 +1,7 @@
 export WANDB_API_KEY="cfbb7b5b972619513ee861d88956b8e497dc71da"
-export http_proxy=http://192.168.32.28:18000 
+export http_proxy=http://192.168.32.28:18000
 export https_proxy=http://192.168.32.28:18000
-export HF_TOKEN="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"
+export HF_TOKEN=${HF_TOKEN}
 export HF_HOME="/mnt/world_foundational_model/gkz/ckpts"
 
 cd /mnt/world_foundational_model/gkz/lmms-eval
@@ -22,4 +22,4 @@ accelerate launch --num_processes 8 --main_process_port 12345 -m lmms_eval \
     --batch_size 1 \
     --log_samples \
     --log_samples_suffix instructblip \
-    --output_path ./logs/ 
+    --output_path ./logs/
--- examples/models/llava_onevision.sh
+++ examples/models/llava_onevision.sh
@@ -1,7 +1,7 @@
 export WANDB_API_KEY="cfbb7b5b972619513ee861d88956b8e497dc71da"
-export http_proxy=http://192.168.32.28:18000 
+export http_proxy=http://192.168.32.28:18000
 export https_proxy=http://192.168.32.28:18000
-export HF_TOKEN="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"
+export HF_TOKEN=${HF_TOKEN}
 export HF_HOME="/mnt/world_foundational_model/gkz/ckpts"
 
 # pip install git+https://github.com/LLaVA-VL/LLaVA-NeXT.git --index-url https://pypi.org/simple
@@ -14,4 +14,4 @@ accelerate launch --num_processes=2 --main_process_port 12399 -m lmms_eval \
     --batch_size=1 \
     --log_samples \
     --log_samples_suffix llava_onevision \
-    --output_path ./logs/ 
+    --output_path ./logs/
