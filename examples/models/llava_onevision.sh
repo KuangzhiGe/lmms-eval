49ab0fac46a8431501a3bb9c3025f5fecef22456
KuangzhiGe <2200013209@stu.pku.edu.cn>
2025-09-22 00:03:48 +0800

    change dataset to local path

--- examples/models/llava_onevision.sh
+++ examples/models/llava_onevision.sh
@@ -10,7 +10,7 @@ export HF_HOME="/mnt/world_foundational_model/gkz/ckpts"
 accelerate launch --num_processes=2 --main_process_port 12399 -m lmms_eval \
     --model=llava_onevision \
     --model_args=pretrained=lmms-lab/llava-onevision-qwen2-7b-ov,conv_template=qwen_1_5,device_map=auto,model_name=llava_qwen \
-    --tasks=physicalmind \
+    --tasks=sat_bench,mmsi_bench,mindcube,threedsr_bench_real,vsr \
     --batch_size=1 \
     --log_samples \
     --log_samples_suffix llava_onevision \
